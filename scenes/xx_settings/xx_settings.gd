16f841740283174fa0ee04339e889eb1bd848c04
Charles-Philippe Bernard <charlesphilippe.bernard@gmail.com>
2024-11-14 02:13:09 +0000

    settings menu ready. need to save settings

---
+++ scenes/xx_settings/xx_settings.gd
@@ -0,0 +1,21 @@
+extends Control
+
+# If your popup is a direct child of this control
+@onready var popup = $MarginContainer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	popup.hide()
+
+func _input(event):
+	if event.is_action_pressed("show_settings"):
+		popup.visible = !popup.visible
+		get_viewport().set_input_as_handled()
+
+
+func _on_close_texture_button_button_down():
+	popup.visible = false
+
+
+func _on_exit_texture_button_button_down():
+	get_tree().quit()
