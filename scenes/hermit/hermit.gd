035ef33ac0f5be9dad8fd2653db8601c63fb526f
Charles-Philippe Bernard <charlesphilippe.bernard@gmail.com>
2025-01-07 19:04:24 +0000

    wip hermit

--- scenes/hermit/hermit.gd
+++ scenes/hermit/hermit.gd
@@ -2,29 +2,37 @@ extends "res://scenes/BaseScene.gd"
 
 func _get_scene_config() -> Dictionary:
 	ActionHandler.CURRENT_HANDLER = self
-	Global.show_hide_item("Bow")
+	var description = """The scene shows the Ermit, a large, jovial man with a bushy white beard and a warm smile, standing confidently in front of his rustic wooden house. 
+ His simple clothing, complete with suspenders, adds to his humble and approachable demeanor. 
+ The house behind him is weathered but sturdy, with planks leaning against it and a bucket nearby, giving the impression of a peaceful, self-sufficient life in the countryside. 
+ The surrounding open landscape adds a serene and welcoming atmosphere."""
+	var actions = ""
+
+	if not Global.has_item("Flute"):
+		description += " A flute is tucked into his front pocket, hinting at a musical side to his character."
+		actions = """
+- If the hero attempts to take the flute:
+  {"_speaker":"001", "_text":"I'll give it to you if you solve this riddle: What walks on four legs in the morning, two legs at noon, and three legs in the evening?", "_action":"RIDDLE"}
+- If the hero correctly answers the riddle (e.g., "man" or similar):
+  {"_speaker":"001", "_text":"The flute is yours now, play it in the centaur forest.", "_action":"FLUTE"}"""
+	else:
+		get_node("/root/hermit/Remaster/ErmitFlute").visible = false
+		get_node("/root/hermit/Original/Flute").visible = false
+
 	return {
 		"compass": {
 			"WEST": "signpost"
 		},
-		"description": "The hero stands in a wide valley, perched atop a grassy hill. In the distance, a towering mountain looms under a clear sky. Nearby, a large rock sits firmly atop the hill, with a bow lying next to it.",
-		"actions": """
-- If the hero attempts to take the bow:
-  {"_speaker":"001", "_text":"Now you need an arrow.", "_action":"BOW"}"""
+		"description": description,
+		"actions": actions
 	}
 
 func execute_action(action):
-	print("Action: " + action)
 	match action:
-		"BOW":
-			Global.take_item_and_animate("Remaster", "Bow", 112, 616)
-			Global.take_item_and_animate("Original", "Bow", 282, 659)
-# O FLITE
-# 287
-# 395
-
-# R
-# 128
-# 387
+		"FLUTE":
+			get_node("/root/hermit/Remaster/ErmitFlute").visible = false
+			get_node("/root/hermit/Remaster/Flute").visible = true
+			Global.take_item_and_animate("Remaster", "Flute", 128, 387)
+			Global.take_item_and_animate("Original", "Flute", 287, 395)
 		_:
 			print("Action not recognized in this scene")
